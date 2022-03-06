05468f7dfd73310c5dff74e48e558908914a04c9
KTangg <k.tangtangtang@gmail.com>
2022-03-07 01:29:29 +0700

    added unit vector

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 14:46:22 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/03/07 01:25:50 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/03/07 01:29:23 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ void	render(t_info *info, t_data *data)
 			if (y + 1 < data->n_row)
 				draw_line_y(y * (data->n_col) + x, data, info, unit);
 			x++;
-			data->start_x = data->start_x + (x *(unit.x.delta_x * MAGNITUDE));
-			data->start_y = data->start_y + (x *(unit.x.delta_y * MAGNITUDE));
+			data->start_x = data->start_x + (unit.x.delta_x * MAGNITUDE));
+			data->start_y = data->start_y + (unit.x.delta_y * MAGNITUDE));
 		}
 		y++;
 		data->start_x = INIT_X + (y *(unit.y.delta_x * MAGNITUDE));
