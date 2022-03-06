c72e39004abe5966acdd0e80f978a67206c5e34a
KTangg <k.tangtangtang@gmail.com>
2022-03-06 14:13:14 +0700

    add print_data f(x)

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:51:03 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/03/06 13:34:16 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/03/06 13:47:32 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,20 @@ static void	usage(void)
 	exit(1);
 }
 
+static void	print_data(t_data data)
+{
+	int	i;
+	int	total;
+
+	total = data.n_row * data.n_col;
+	i = 0;
+	while (i < total)
+	{
+		ft_printf("Data: %d, %d\n", data.attribute[i], data.color[i]);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	void	*mlx;
@@ -30,6 +44,7 @@ int	main(int argc, char **argv)
 		usage();
 	mlx = mlx_init();
 	parsing_file(argv[1], &data);
+	print_data(data);
 	free_data(&data);
 	free(mlx);
 	return (0);
